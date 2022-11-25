276c57de29d1dd552fe2b1840eb3d51a1d6a0bc0
Zhao, Zhiqiang <zhiqiang.zhao@intel.com>
2022-11-25 05:20:29 -0500

    print log information for debug
    Signed-off-by: Zhao, Zhiqiang <zhiqiang.zhao@intel.com>

--- DeviceSecurityPkg/Library/TestSignListLib/TestSignListLib.c
+++ DeviceSecurityPkg/Library/TestSignListLib/TestSignListLib.c
@@ -24,18 +24,29 @@ Traverse_SignatureList (
   if (SignatureList != NULL) {
     DbList = SignatureList;
     DbSize = SignatureListSize;
+    printf("SignatureListSize out of while- 0x%x\n", SignatureListSize);
+    printf("DbList->SignatureListSize out of while- 0x%x\n", DbList->SignatureListSize);
     while ((DbSize > 0) && (SignatureListSize >= DbList->SignatureListSize)) {
+      printf("DbSize at begin of while- 0x%x\n", DbSize);
+      printf("DbList->SignatureListSize at begin of while- 0x%x\n", DbList->SignatureListSize);
+      printf("DbList->SignatureSize at begin of while- 0x%x\n", DbList->SignatureSize);
       if (!CompareGuid (&DbList->SignatureType, &gEfiCertX509Guid)) {
+        printf("DbSize at begin of CompareGuid block- 0x%x\n", DbSize);
+        printf("DbList->SignatureListSize at begin of CompareGuid block- 0x%x\n", DbList->SignatureListSize);
         DbSize -= DbList->SignatureListSize;
         DbList  = (EFI_SIGNATURE_LIST *)((UINT8 *)DbList + DbList->SignatureListSize);
+        printf("DbSize at end of CompareGuid block- 0x%x\n", DbSize);
+        printf("DbList->SignatureListSize at end of CompareGuid block- 0x%x\n", DbList->SignatureListSize);
         continue;
       }
       ASSERT (DbList->SignatureHeaderSize == 0);
       SiglistHeaderSize = sizeof (EFI_SIGNATURE_LIST) + DbList->SignatureHeaderSize;
       Cert = (EFI_SIGNATURE_DATA *)((UINT8 *)DbList + SiglistHeaderSize);
       CertCount = (DbList->SignatureListSize - SiglistHeaderSize) / DbList->SignatureSize;
+      printf("CertCount in while block- 0x%x\n", CertCount);
 
       for (Index = 0; Index < CertCount; Index ++) {
+        printf("Index in for block- 0x%x\n", Index);
         Data = Cert->SignatureData;
         DataSize = DbList->SignatureSize - sizeof (EFI_GUID);
 
@@ -48,6 +59,9 @@ Traverse_SignatureList (
 
       DbSize -= DbList->SignatureListSize;
       DbList  = (EFI_SIGNATURE_LIST *)((UINT8 *)DbList + DbList->SignatureListSize);
+      printf("DbSize at end of while- 0x%x\n", DbSize);
+      printf("DbList->SignatureListSize at end of while- 0x%x\n", DbList->SignatureListSize);
+      printf("DbList->SignatureSize at end of while- 0x%x\n", DbList->SignatureSize);
     }
   }
 }
