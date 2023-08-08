6feebc1efb3bc7fc62509016d88fc6f8c465ea3d
Zhao, Zhiqiang <zhiqiang.zhao@intel.com>
2023-08-08 16:54:46 +0800

    Update test script to check the errorkeyword should not appear in event log.
    Signed-off-by: Zhao, Zhiqiang <zhiqiang.zhao@intel.com>

--- DeviceSecurityTestPkg/Test/test_script/find_keyword.py
+++ DeviceSecurityTestPkg/Test/test_script/find_keyword.py
@@ -7,54 +7,83 @@ class logprocessor:
 
 
 
-	def __init__(self, logPath, keywordPath):
-		print("========================== new test case begin ============================\n") 
-		self.logPath = logPath
-		self.keywordPath = keywordPath
-		self.keywordLines = self.GetKeywords(self.keywordPath)
-		self.LogContent = self.GetLogContent(self.logPath)
+	def __init__(self, LogPath, KeywordPath):
+		print("========================== new test case begin ============================\n")
+		self.LogPath = LogPath
+		self.KeywordPath = KeywordPath
+		self.ExpectKeywordLines, self.ErrorKeywordLines = self.GetKeywords(self.KeywordPath)
+		self.LogContent = self.GetLogContent(self.LogPath)
 		pass
 		
-	def Process(self):
-		processed = False
+	def Check(self):
+		Result = False
 		count = 0;
-		print("\n*************try to find keyword in the log ......\n") 
-		for line in self.keywordLines:	
+		print("\n*************try to find Keyword in the log ......\n")
+		for line in self.ExpectKeywordLines:
 			if(line in self.LogContent):
-				processed=True
+				Result=True
 			else:
-				processed = False
-				print("\n***Can not find the keyword below in the log!!!") 
+				Result = False
+				print("\n***Can not find the following ExpectKeyword in the log!!!\n")
 				print(line)
 				break
-	
-		if (processed):
-			print("\n***keywords are all found in the log successfully :-)") 
-			print("*** So this test case passed :-)\n") 
-		else:
-			print("\n***found some errors in the log!!!") 
-			print("*** So this test case failed :(\n") 
+
+		if (not Result):
+			print("\n***not found ExpectKeyword in the log!!!\n")
+			print("*** So this test case failed :(\n")
+			print("******************************************************\n")
+			return
+
+		for line in self.ErrorKeywordLines:
+			if(line not in self.LogContent):
+				Result=True
+			else:
+				Result = False
+				print("\n***find the following ErrorKeyword in the log!!!\n")
+				print(line)
+				break
 			
-		print("******************************************************\n") 
+		if (Result):
+			print("\n***found all ExpectKeywords and not found any ErrorKeyword in the log :-)")
+			print("*** So this test case passed :-)\n")
+		else:
+			print("\n***found ErrorKeyword in the log!!!")
+			print("*** So this test case failed :(\n")
+			
+		print("******************************************************\n")
 		pass
 
-	def GetKeywords(self, keywordPath):
-		keywordFile = open(keywordPath, 'r')
-		keywordLines = []
-		print("--------------------------keywords list-----------------------------\n") 
-		for line in keywordFile.readlines():
+	def GetKeywords(self, KeywordPath):
+		KeywordFile = open(KeywordPath, 'r')
+		AllKeywordLines = KeywordFile.readlines()
+		ExpectKeywordLines = []
+		ErrorKeywordLines = []
+		HaveErrorKeyword = False
+
+		TargetKeywordLines = ExpectKeywordLines
+		print("--------------------------ExpectKeywords list start------------------------\n")
+		for line in AllKeywordLines:
 			line = line.strip()
 			if (not line):
 				continue
+			if (line.startswith('ErrorKeyword')):
+				HaveErrorKeyword = True
+				TargetKeywordLines = ErrorKeywordLines
+				print("\n------------------------ExpectKeywords list end--------------------------\n")
+				print("\n------------------------ErrorKeywords list start-------------------------\n")
+				continue
 			print(line)
-			keywordLines.append(line)
-		print("\n------------------------keywords list end--------------------------\n") 
-		keywordFile.close()
-		return keywordLines
+			TargetKeywordLines.append(line)
+		if (HaveErrorKeyword):
+			print("\n------------------------ErrorKeywords list end---------------------------\n")
+		else:
+			print("\n------------------------ExpectKeywords list end--------------------------\n")
+		KeywordFile.close()
+		return ExpectKeywordLines, ErrorKeywordLines
 		pass
 		
-	def GetLogContent(self, logPath):
-		LogFile = codecs.open(logPath, 'r', 'utf-16le')
+	def GetLogContent(self, LogPath):
+		LogFile = codecs.open(LogPath, 'r', 'utf-16le')
 		LogFileLines = []
 		for line in LogFile.readlines():
 			line = line.strip()
@@ -68,14 +97,14 @@ class logprocessor:
 if __name__ == '__main__':
 
 	if len(sys.argv) != 3:
-		print("Usage:\n%s" % "  python find_keyword.py <logFile> <keywordFile> " )
+		print("Usage:\n%s" % "  python find_Keyword.py <logFile> <KeywordFile> " )
 		sys.exit(0)
 	
-	logPath = sys.argv[1]
-	keywordPath = sys.argv[2]
+	LogPath = sys.argv[1]
+	KeywordPath = sys.argv[2]
 	
-	my_logprocessor = logprocessor(logPath, keywordPath)
-	my_logprocessor.Process()
+	my_logprocessor = logprocessor(LogPath, KeywordPath)
+	my_logprocessor.Check()
 	
 	pass
 
